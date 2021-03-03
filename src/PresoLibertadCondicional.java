dcc4b58d8be078d425ba22015c002768db8ecfe0
Faustito <Faustito@MSI>
2021-03-03 15:23:37 -0500

    Se corrigieron los errores de la clase calculoTiempoRestante, pero tenemos que probar su funcionalidad.

--- src/PresoLibertadCondicional.java
+++ src/PresoLibertadCondicional.java
@@ -10,7 +10,6 @@ import java.util.GregorianCalendar;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Faustito
@@ -23,29 +22,16 @@ public class PresoLibertadCondicional extends PresoProcesoLibertad {
 
     //Métodos
     //provisional 
-    public void calculoTiempoRestante(int dia, int mes, int anio) {
-        int diaA, mesA, anioA;
-        String diaSA, mesSA, anioSA;
-        //SimpleDateFormat dateFormat=new SimpleDateFormat("YYYY-MM-DD");
-        //instanciamos un objeto calendario
-        Calendar c = Calendar.getInstance();
-        //sacamos la fecha actual por partes en dia mes y anio
-        diaA = c.get(Calendar.DAY_OF_MONTH);
-        diaSA = String.valueOf(diaA);
-        mesA = c.get(Calendar.MONTH);
-        mesSA = String.valueOf(mesA);
-        anioA = c.get(Calendar.YEAR);
-        anioSA = String.valueOf(anioA);
-        //definimos la fehcha inicial
-        Calendar fechaInicial = new GregorianCalendar();
-        fechaInicial.set(anio, mes, dia);
-        //definimos la fecha final
-        Calendar fechaFinal = new GregorianCalendar();
-        fechaFinal.set(diaA, mesA, anioA);
-        //sacamos la resta de las dos fechas en milisegundos para obtener los dias exactos que tiene el preso para estar libre
-        c.setTimeInMillis(fechaFinal.getTime().getTime() - fechaInicial.getTime().getTime());
-        System.out.println("Numero de dias que tiene restantes de condena:" + c.get(Calendar.DAY_OF_YEAR));
+    public int calculoTiempoRestante(Date fechaIngreso) {
+        long tiempoFin, tiempoIngreso, dias;
+        tiempoIngreso = fechaIngreso.getTime();
+        Date fechaActual = new Date();
+        tiempoFin = fechaActual.getTime();
+        dias = (tiempoFin - tiempoIngreso) / (1000 * 60 * 60 * 24);
+        return (int) dias;
+
     }
+
     public String motivoLibCond(String motivoLibCond) {
         return "El motivo por el cual se le cede al preso el beneficio de la libertad condicional es el siguiente:" + motivoLibCond;
     }
